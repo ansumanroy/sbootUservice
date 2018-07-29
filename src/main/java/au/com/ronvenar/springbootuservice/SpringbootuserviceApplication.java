634ddfa82787ad4ccfcc2ac4070521ca05817c2d
ansumanroygit@gmail.com <Ansuman.Roy@Ronvenar.com.au>
2018-07-29 20:27:11 +1000

    initial spring starter app commits

---
+++ src/main/java/au/com/ronvenar/springbootuservice/SpringbootuserviceApplication.java
@@ -0,0 +1,12 @@
+package au.com.ronvenar.springbootuservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootuserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootuserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/au/com/ronvenar/springbootuservice/SpringbootuserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package au.com.ronvenar.springbootuservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootuserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
